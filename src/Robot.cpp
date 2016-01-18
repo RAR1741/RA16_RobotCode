530f93be483db9ccc0280097221320ca9defded8
randomtestfive <taylorhorne512@gmail.com>
2016-01-18 18:06:49 +0000

    Tried adding vision. Commented out.

--- src/Robot.cpp
+++ src/Robot.cpp
@@ -2,6 +2,7 @@
 //#include <string>
 #include <iostream>
 #include "CameraServer.h"
+#include <unistd.h>
 
 using namespace std;
 
@@ -17,16 +18,24 @@ private:
 	const std::string autoNameCustom = "My Auto";
 	std::string autoSelected;
     AnalogGyro * gyro;
+    const char * const JAVA = "/usr/local/frc/JRE/bin/java";
+    const char * const GRIP_JAR = "/home/lvuser/GRIP.jar";
+    const char * const GRIP_PROJECT = "/home/lvuser/everything.grip";
+
+    std::shared_ptr<NetworkTable> grip = NetworkTable::GetTable("GRIP");
 
 public:
 	Robot()
 	{
+        //execl(JAVA, JAVA, "-jar", GRIP_JAR, GRIP_PROJECT, nullptr);
+            //wpi_setErrnoErrorWithContext("Failed to run GRIP");
+
 		motor1 = NULL;
 		joy1=NULL;
 		lw = NULL;
 		chooser = NULL;
 		cameraUSB = NULL;
-		gyro = NULL;
+		gyro = new AnalogGyro(1);// = NULL;
 	};
 
 	void RobotInit()
@@ -34,17 +43,17 @@ public:
 		lw = LiveWindow::GetInstance();
 		chooser = new SendableChooser();
 		cameraUSB = CameraServer::GetInstance();
-		//cameraUSB->StartAutomaticCapture("cam0");
+		cameraUSB->StartAutomaticCapture("cam0");
 		cameraUSB->SetQuality(2);
 		cameraUSB->StartAutomaticCapture();
 
-		gyro = new AnalogGyro(1);
+		//gyro = new AnalogGyro(1);
 
 		//chooser->AddDefault(autoNameDefault, (void*)&autoNameDefault);
 		//chooser->AddObject(autoNameCustom, (void*)&autoNameCustom);
 
 		SmartDashboard::PutData("Auto Modes", chooser);
-		lw->AddSensor((const std::string)"Main", 0, gyro);
+		//lw->AddSensor((const std::string)"Main", 0, gyro);
 		joy1 = new Joystick(0);
 		motor1 = new Victor(0);
 	}
@@ -86,13 +95,22 @@ public:
 
 	void TeleopInit()
 	{
-
+		cout << "thing" << endl;
 	}
 
 	void TeleopPeriodic()
 	{
-		motor1->Set(joy1->GetX());
-		cout << gyro->GetAngle() << endl;
+		//motor1->Set(joy1->GetX());
+		//cout << gyro->GetAngle() << endl;
+
+	    auto areas = grip->GetNumberArray("myContoursReport/area", llvm::ArrayRef<double>()),
+	         xs    = grip->GetNumberArray("myContoursReport/x",    llvm::ArrayRef<double>()),
+	         ys    = grip->GetNumberArray("myContoursReport/y",    llvm::ArrayRef<double>());
+
+	    for (unsigned int i = 0; i < areas.size(); i++) {
+	        double area = areas[i], x = xs[i], y = ys[i];
+	        std::cout << "Got contour: area=" << area << ", x=" << x << ", y=" << y << std::endl;
+	    }
 	}
 
 	void TestPeriodic()
