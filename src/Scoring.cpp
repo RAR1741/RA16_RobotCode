16470242f2d4613a6b9d6ff488483ff6031ea001
randomtestfive <taylorhorne512@gmail.com>
2016-01-24 01:19:33 +0000

    scoring kinda finished

--- src/Scoring.cpp
+++ src/Scoring.cpp
@@ -14,5 +14,58 @@ Scoring::Scoring(CANTalon *aMotor, CANTalon *tMotor, CANTalon *lMotor, CANTalon 
 	TensionMotor = tMotor;
 	LFlyMotor = lMotor;
 	RFlyMotor = rMotor;
+	AngleMotor->SetControlMode(CANTalon::kPosition);
 	state = State::kWaiting;
+}
+
+void Scoring::Update()
+{
+	if(state == kLoading)
+	{
+		//Do loading things
+		//set to kReady if loaded
+	}
+	else if(state == kShooting)
+	{
+		SetFlySpeed(-1);
+		//Do shooting things
+		//set to kWaiting if done
+	}
+}
+
+void Scoring::Load()
+{
+	if(state == kWaiting)
+	{
+		state = kLoading;
+	}
+}
+
+void Scoring::Fire()
+{
+	if(state == kReady)
+	{
+		state = kShooting;
+	}
+}
+
+void Scoring::SetFlySpeed(float speed)
+{
+	LFlyMotor->Set(-speed);
+	LFlyMotor->Set(speed);
+}
+
+void Scoring::SetAngle(float angle)
+{
+	AngleMotor->Set(angle);
+}
+
+float Scoring::GetAngle()
+{
+	return AngleMotor->GetEncPosition();
+}
+
+Scoring::State Scoring::GetState()
+{
+	return state;
 }
