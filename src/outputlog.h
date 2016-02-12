924c273528bd86975c6f676d5a5a037f557932ae
randomtestfive <taylorhorne512@gmail.com>
2016-02-12 01:09:53 +0000

    start moving out of robot.cpp for cleanup

---
+++ src/outputlog.h
@@ -0,0 +1,19 @@
+/*
+ * outputlog.h
+ *
+ *  Created on: Feb 11, 2016
+ *      Author: taylo
+ */
+
+#ifndef SRC_OUTPUTLOG_H_
+#define SRC_OUTPUTLOG_H_
+
+using namespace std;
+
+class OutputLog
+{
+public:
+	void Troll(ostream & out);
+};
+
+#endif /* SRC_OUTPUTLOG_H_ */
