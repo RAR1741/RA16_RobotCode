4d4623513cfbcc910ce27126920284c3b3d3c6d4
randomtestfive <taylorhorne512@gmail.com>
2016-02-12 01:14:20 +0000

    clean up robot.cpp

--- src/Robot.cpp
+++ src/Robot.cpp
@@ -12,6 +12,7 @@
 #include "Targeting.h"
 #include "Config.h"
 #include "Scoring.h"
+#include "outputlog.h"
 
 using namespace std;
 
@@ -58,6 +59,7 @@ private:
     Victor * rin;
     //////////////////////////
     Scoring * score;
+    OutputLog * logthing = new OutputLog();
 
 
     std::string profile = "everything";
@@ -175,7 +177,7 @@ public:
 		//scoring->SetControlMode();
 		ReloadConfig();
 		//StartLogging("init");
-		OutputTroll(cout);
+		logthing->Troll(cout);
 	}
 
 
@@ -378,46 +380,6 @@ public:
 	{
 		Config::LoadFromFile("/home/lvuser/config.txt");
 		cout << Config::GetSetting("wow", 0) << endl;
-	}
-
-	void OutputTroll(ostream & out)
-	{
-		out
-		<< "                                      ....... .\n"
-		"                      ,7IB@MMMMMMMMMMMMMMMMMMM@@@MMMMMMMMMM,               \n"
-		"                ,I#MMMMMMM$ZI7Yii,,.,....     ..         :#MM              \n"
-		"             YMMMWoci,.      ..,:iii,,.........,,:ii.       MMo            \n"
-		"           EMM9       ,,:,...:;;ci,    ...::;;YYY;;icY;      2M#           \n"
-		"          MM;     .;:,.,,ii;;         Ui     ...,vSUv;iY;.    AMz          \n"
-		"         ,M      SI        Yi        Ai            .v;;,..     @M          \n"
-		"         MM      .          S            ,C6SotXi.     .        MM         \n"
-		"        MM.        ioAU1;   ,         YMMMbiMMMMMMM@i            MM;       \n"
-		"      2MM  ,..,; :MMMMMMMMM@.       .MM,   ;MMMMMM#@MM iSY    .i,iMMMv     \n"
-		"     MMv.YI.      ,iYt68B@MMMMMb    cMMt@MM$Y.C.  iobC   .,CZEb;   .bMM.   \n"
-		"    MM.,:Y..YoY,            Mo        S$:     CM#.     nMMM@1zbMMQ    nMt  \n"
-		"    M@, , vMM#E@MMX.@2      M.                 .$MMMMMMMZ.  @    MM  . 7M, \n"
-		"    MMi Y     I iMMMM,    MMM         vi                 .UMM$    MM i  M@ \n"
-		"    SM9  C   :M.       iMMb          :@MMMvi:.,.      9MMMZ oMMMn MM ,  MM \n"
-		"     MMC.7,  MMC    .cCMMMz      YMMMM, #M      .7$MMMMo    MM i;tM. 7 ;M7 \n"
-		"      MM .Y MM@M$.        MM; A. .;. . .M   7WMMMM$,M$    7MMY   X, C iM#  \n"
-		"       M@   MMMCMMM@i      .#MW      .vE$MMM#A:     MM1MMMMM7       ,MMo   \n"
-		"       zM   MME M1 SMMMMMM@o8@$@MMMMMM#Ii.Mt     ;MMMMM: MM        0MM     \n"
-		"        MY .MM6 Mi  MC    cM8    M@       ZMi$MMMMMQM;  MM        MM,      \n"
-		"        Mz  MMMMMM6EMz.   ,M.    M#  .;z@MMMMMM#7   MoYMM        MM        \n"
-		"        Mz  MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMCM@       tMMS        oM        \n"
-		"        M;  EMAMMMMMMMMMMMMMMMMMMMMMZ7i    ZM     8MM8         XMC     \n"
-		"       .M.   Mb9M MM :M@ .YMM     .M        @M ;MMMz          MM7   \n"
-		"       SM    ,MMM  M7 ,M    M     .M       ,$MMMb       i.  @MM  \n"
-		"       MM      6MMMMM$nMMc. MM.:::IMMW#MMMMMW,   .:;c1SYioMMM.\n"
-		"       M@          .c6WE#MMMMMMMMMMQ$Q8C:     iYCYvi,,X#MM9\n"
-		"       M2  S.  i8,                       .;2Uo;,..,2@MMZ\n"
-		"       Mc  .X,   v1Xv:.           ,,vXoUASv.   v$MM@Y\n"
-		"       M;    7Iz;............,.,,:::..     .8MMM2.\n"
-		"       MM       .,:::........           ;@MM$:\n"
-		"        MMz                     .z@MMMMMMb.\n"
-		"         iMMME7.       .,i1Z#MMMMMMES;.\n"
-		"            C@MMMMMMMMMMMM@Wn;.\n"
-		"\n";
 	}
 };
 
