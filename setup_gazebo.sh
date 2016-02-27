e5877f8900204321e684ff9cb694ee57abe55902
tricktrap <patrick.frampton@gmail.com>
2016-02-27 22:35:22 +0000

    Attempt vagrant setup for Gazebo

---
+++ setup_gazebo.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+# Sets up Gazebo on a ubuntu machine.
+wget -O frcsim-installer.sh first.wpi.edu/FRC/roborio/release/frcsim-installer.sh && chmod a+x frcsim-installer.sh && ./frcsim-installer.sh INSTALL
